fc5a6a675897771806d945c53e2ead3e6df6bee3
igor2000xp <igorring@gmail.com>
2024-06-22 08:27:39 +0200

    fix: marketing component

--- src/app/pages/main/data/marketing-data.ts
+++ src/app/pages/main/data/marketing-data.ts
@@ -1,15 +1,33 @@
+// export const marketingData = [
+//   {
+//     title: 'Increasing Prosperity With Positive Thinking',
+//     description: 'Art, Design',
+//     imageUrl: './assets/image.svg',
+//     tags: ['Art', 'Design']
+//   },
+//   {
+//     title: 'Motivation Is The First Step To Success',
+//     description: 'Culture',
+//     imageUrl: './assets/image-4.svg',
+//     tags: ['Culture']
+//   },
+//   // add 6 more objects for a total of 8
+// ];
 export const marketingData = [
   {
-    title: 'Increasing Prosperity With Positive Thinking',
-    description: 'Art, Design',
-    imageUrl: './assets/image.svg',
-    tags: ['Art', 'Design']
+    image: 'assets/image-1.svg',
+    tags: ['Culture', 'Design', 'Art'],
+    title: 'Success Steps For Your Personal Or Business Life'
   },
   {
-    title: 'Motivation Is The First Step To Success',
-    description: 'Culture',
-    imageUrl: './assets/image-4.svg',
-    tags: ['Culture']
+    image: 'assets/image-2.svg',
+    tags: ['Culture', 'Design', 'Art'],
+    title: 'Motivation Is The First Step To Success'
   },
-  // add 6 more objects for a total of 8
+  {
+    image: 'assets/image-3.svg',
+    tags: ['Culture', 'Design', 'Art'],
+    title: 'Increasing Prosperity With Positive Thinking'
+  },
+  // Add more cards as needed
 ];
